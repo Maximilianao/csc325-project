868274bf01aaca112117789f74d8aee856c35850
thomas <thomasgarafano1@gmail.com>
2025-10-29 19:44:09 -0400

    added firebase setup

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,12 @@
 module org.csc325.semesterproject.smartflashcards {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires firebase.admin;
+    requires com.google.auth;
+    requires com.google.auth.oauth2;
+    requires google.cloud.firestore;
+    requires google.cloud.core;
+    requires com.google.api.apicommon;
 
     opens org.csc325.semesterproject.smartflashcards to javafx.fxml;
     exports org.csc325.semesterproject.smartflashcards;
