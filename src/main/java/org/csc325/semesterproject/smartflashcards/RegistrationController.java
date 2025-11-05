5f0eaeb4c010160225e0f9f566adffcf2ee97ebd
Thomas Garafano <thomasgarafano1@gmail.com>
2025-11-05 14:10:54 -0500

    connected registraton to firebase

--- src/main/java/org/csc325/semesterproject/smartflashcards/RegistrationController.java
+++ src/main/java/org/csc325/semesterproject/smartflashcards/RegistrationController.java
@@ -1,5 +1,6 @@
 package org.csc325.semesterproject.smartflashcards;
 
+import com.google.firebase.auth.UserRecord;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,6 +11,27 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.cloud.firestore.WriteResult;
+import com.google.firebase.auth.FirebaseAuthException;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+
+import javafx.scene.control.Alert;
+
+import javafx.scene.control.TextArea;
+
 
 public class RegistrationController {
     @FXML
@@ -37,7 +59,42 @@ public class RegistrationController {
     }
 
     @FXML
-    public void signUp() {
+    public boolean signUp() {
+        UserRecord.CreateRequest request = new UserRecord.CreateRequest()
+                .setEmail(emailInputField.getText())
+                .setEmailVerified(false)
+                .setPassword(passwordInputField.getText())
+                //.setPhoneNumber(phoneNumberTextField.getText())
+                .setUid(userInputField.getText())
+                .setDisabled(false);
+
+
+
+        UserRecord userRecord;
+        try {
+            userRecord = FlashcardApplication.fauth.createUser(request);
+            System.out.println("Successfully created new user with Firebase Uid: " + userRecord.getUid()
+                    + " check Firebase > Authentication > Users tab");
+            addData();
+            return true;
+
+        } catch (FirebaseAuthException ex) {
+            // Logger.getLogger(FirestoreContext.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Error creating a new user in the firebase");
+            return false;
+        }
+
+    }
+
+    public void addData() {
+
+        DocumentReference docRef = FlashcardApplication.fstore.collection("Passwords").document(userInputField.getText());
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("Password", passwordInputField.getText());
+
+        //asynchronously write data
+        ApiFuture<WriteResult> result = docRef.set(data);
     }
 
     @FXML
@@ -51,5 +108,10 @@ public class RegistrationController {
         } catch (Exception e) {
             System.out.println("Error loading login screen.");
         }
+    }
+
+    @FXML
+    void signUpButtonClicked(ActionEvent event) {
+        signUp();
     }
 }
