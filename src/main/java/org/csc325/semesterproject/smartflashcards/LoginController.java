489b3d3ce07903aa688c28fc4a2be61fb3374938
Thomas Garafano <thomasgarafano1@gmail.com>
2025-11-06 12:46:18 -0500

    connected login to firebase

--- src/main/java/org/csc325/semesterproject/smartflashcards/LoginController.java
+++ src/main/java/org/csc325/semesterproject/smartflashcards/LoginController.java
@@ -9,6 +9,27 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.cloud.firestore.WriteResult;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.UserRecord;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class LoginController {
     @FXML
@@ -34,6 +55,55 @@ public class LoginController {
             currentScene.setRoot(root);
         } catch (Exception e) {
             System.out.println("Error loading registration screen.");
+        }
+    }
+
+
+    public void signIn() {
+        ApiFuture<QuerySnapshot> future =  FlashcardApplication.fstore.collection("Passwords").get();
+        List<QueryDocumentSnapshot> documents;
+        boolean passwordMatch = false;
+        boolean userMatch = false;
+        try
+        {
+            documents = future.get().getDocuments();
+            if(documents.size()>0)
+            {
+                System.out.println("Getting (reading) data from firabase database....");
+
+                for (QueryDocumentSnapshot document : documents) {
+                    if (document.getData().get("Password").equals(passwordInputField.getText())) {
+                        System.out.println(document.getData().get("Password"));
+                        passwordMatch = true;
+                    }
+                    else{
+                        System.out.println("password wrong");
+                    }
+                    if(document.getId().equals(userInputField.getText())){
+                        System.out.println(document.getId());
+                        userMatch = true;
+                    }
+                    else{
+                        System.out.println("username wrong");
+                    }
+                    if(passwordMatch == true && userMatch == true){
+                        try {
+                            FlashcardApplication.setRoot("primary");
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                System.out.println("No data");
+            }
+
+        }
+        catch (InterruptedException | ExecutionException ex)
+        {
+            ex.printStackTrace();
         }
     }
 }
