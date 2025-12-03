7408c5be3d1ce2b6e8e1535e7b7970a721e43f4a
Thomas Garafano <thomasgarafano1@gmail.com>
2025-12-03 11:50:23 -0500

    added psuedo code for quiz

---
+++ src/main/java/org/csc325/semesterproject/smartflashcards/quizController.java
@@ -0,0 +1,15 @@
+// psuedocode
+
+// imports
+//
+//
+// class name
+//
+//
+// @FXML
+// createQuestion() method
+//
+// add words to list
+// add definitions to other list
+// when displaying question remove from lists
+// add 3 random other words to display
