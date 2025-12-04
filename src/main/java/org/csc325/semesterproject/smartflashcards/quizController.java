8e7a16b9e677e1a7715cb002f055c9a267d82c4d
Thomas Garafano <thomasgarafano1@gmail.com>
2025-12-04 12:13:31 -0500

    initilized quiz screen

--- src/main/java/org/csc325/semesterproject/smartflashcards/quizController.java
+++ src/main/java/org/csc325/semesterproject/smartflashcards/quizController.java
@@ -1,15 +1,4 @@
-// psuedocode
+package org.csc325.semesterproject.smartflashcards;
 
-// imports
-//
-//
-// class name
-//
-//
-// @FXML
-// createQuestion() method
-//
-// add words to list
-// add definitions to other list
-// when displaying question remove from lists
-// add 3 random other words to display
+public class quizController {
+}
