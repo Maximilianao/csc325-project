5bcf4568179643a194257ba065a45def3e620b4e
Thomas Garafano <thomasgarafano1@gmail.com>
2025-12-05 09:50:38 -0500

    added arraylist to quizcontroller and added exists_placeholder to studyconroller block

--- src/main/java/org/csc325/semesterproject/smartflashcards/quizController.java
+++ src/main/java/org/csc325/semesterproject/smartflashcards/quizController.java
@@ -1,4 +1,24 @@
 package org.csc325.semesterproject.smartflashcards;
 
+import com.google.cloud.firestore.*;
+import javafx.application.Platform;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseButton;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import java.util.*;
+
 public class quizController {
+
+    private ArrayList<String> def = new ArrayList<String>();
+    private ArrayList<String> words = new ArrayList<String>();
+
+
 }
