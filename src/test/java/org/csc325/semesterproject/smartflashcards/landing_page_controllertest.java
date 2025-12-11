7e6c9377cd4b64a581bd4136b89a246ada1915c7
Maximilianao <maxicarrion07@gmail.com>
2025-12-10 19:35:39 -0500

    added testing

---
+++ src/test/java/org/csc325/semesterproject/smartflashcards/landing_page_controllertest.java
@@ -0,0 +1,18 @@
+package org.csc325.semesterproject.smartflashcards;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class landing_page_controllertest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void initialize() {
+    }
+
+
+}
+
---
+++ src/test/java/org/csc325/semesterproject/smartflashcards/logincontrollertest.java
@@ -0,0 +1,24 @@
+package org.csc325.semesterproject.smartflashcards;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class logincontrollertest {
+    @BeforeEach
+    void signIn() {
+    }
+
+    @Test
+    void initialize() {
+    }
+
+    @Test
+    void viewRegistrationScreen(){
+    }
+
+
+
+
+}
+
+
