3d425d59b77b6e276eecd82baeab77d447cd8a08
thomas <thomasgarafano1@gmail.com>
2025-12-10 19:15:34 -0500

    added junit test to quiz

---
+++ src/test/java/org/csc325/semesterproject/smartflashcards/quizControllerTest.java
@@ -0,0 +1,17 @@
+package org.csc325.semesterproject.smartflashcards;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class quizControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void initialize() {
+    }
+}
